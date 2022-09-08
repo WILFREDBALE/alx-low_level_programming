ac9a1e26709acb09f986e1459b5499ec558bff9c
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-08 14:01:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	print ("with proper grammer, but the outcome is a price of art,\n")
+		return (0):  }
