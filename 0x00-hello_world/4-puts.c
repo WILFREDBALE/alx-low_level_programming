dccae5655832a7d0a3cf932cc61e645600e856c7
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-08 17:08:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main -entry point
+ * main - print the string in the put function
  *
- * Return: 0 after printing the function
- */
-int main (void)
-{puts ("\"programming is like building a multilingual puzzle"); return (0)
-}
+ *  Description: using the main function 
+ *  this program prints "programming is like building a multilingual puzzle
+ *  Return: 0
+ *  */
+ int main (void)
+ {
+	 puts{"\" programming is like building a multilingual puzzle");
+	 return (0);
+
+ }
