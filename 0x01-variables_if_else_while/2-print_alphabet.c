c281b8c897e31f0507fc986937d1b6cc1b91c65a
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-11 02:30:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (seccess/correct)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putcher('\n');
+
+return (0);
