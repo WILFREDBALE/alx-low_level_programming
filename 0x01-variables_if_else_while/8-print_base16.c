146bf662ff5c6e10cff8f774fdeb0e073c737165
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-11 16:24:11 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 char c;
-for (c = '0' c <= '9'; c++)
+for (c = '0'; c <= '9'; c++); 
 {
 putchar(c);
 }
-for (c = 'a'; c <= 'f'; c++)
+for (c = 'a'; c <= 'f'; c++);
 {
 putchar(c);
 }
-putchar('%d\n');
+putchar('\n');
 return (0);
 }
 
