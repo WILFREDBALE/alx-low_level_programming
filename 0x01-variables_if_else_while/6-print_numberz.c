338c44ca6a5261e06a3b899b5821e9edbafc3e06
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-12 11:51:37 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
-clear
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
