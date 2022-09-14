b59c59e97f70d5d21e847cb94601c4e17501d238
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-13 21:26:22 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * print_sign - function checks for a sign of a number
+ * @n: is the int to be usedfor the argument of the function
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+{putchar('a');
+return (1);
+}
+else if (n < 0)
+{
+putchar('-')
+return (-1);
+}
+else
+{
+putchar('0');
+return (0);
+}
+}
