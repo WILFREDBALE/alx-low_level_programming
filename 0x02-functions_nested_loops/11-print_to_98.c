3b50d1d55a62345b28208e9e7fd31f4ed026616e
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 14:36:22 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,19 +4,21 @@
  * print_to_98 -this prints all natural number from n to 98
  * seperated by comma and space
  * @n: represents the number from which counting begins
+ * Return: 0 or 1
  */
 void print_to_98(int n)
 {
-if (n >= 98)
-{
 while (n > 98)
-printf(""%d," n--);
-printf("%d\n", n);
-}
-else
 {
-while (n < 98)
-printf(""%d,", n++);
-printf("%d\n", n);
+printf("%i, ", n);
+n++;
 }
+while (n > 98)
+{
+printf("%i, ", n);
+n--;
+}
+print("98");
+putchar('\n');
+
 }
