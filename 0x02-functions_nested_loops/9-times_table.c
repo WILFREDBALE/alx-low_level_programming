466c5a58a3239bb0cd9a9cbb0f39e8f0fff61616
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 10:03:04 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,9 @@
  * times_table - prints the 9 timestable, starting with 0.
  */
 void times_table(void)
-{int num, mult, product; 
+{
+
+int num, mult, product; 
 
 for (num = 0; num <= 9; num = num + 1)
 {
