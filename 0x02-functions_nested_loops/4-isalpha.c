5abee2f13814ec895fe101b849b581222f3a8fa8
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-13 20:43:23 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _isalpha - function to check if c is a letter, lowercsae or uppercase
+ * &: is the int to be used for the argument of the function
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+if (( c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+return (1);
+}
