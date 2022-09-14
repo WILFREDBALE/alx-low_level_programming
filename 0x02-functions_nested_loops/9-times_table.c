ffbd53d7825cf796486efef6300ebd6697652268
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 08:31:26 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,8 +4,9 @@
  */
 void times_table(void)
 {
-	int num, mult, product;
-	for (num = 0; num <=9; num = num + 1)
+	int num, mult, product; 
+
+	for (num = 0; num <= 9; num = num + 1)
 	{
 		_putchar('0');
 		for (mult = 1; mult <= 9; mult++)
