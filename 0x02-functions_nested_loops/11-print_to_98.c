f03dcfe55bd6b4ec4b862bcda90374ba6c6af67f
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 13:41:44 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 -this prints all natural number from n to 98
  * seperated by comma and space
@@ -9,7 +10,7 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-printf("%d,", n+=);
+printf("%d,", n++);
 printf("%d\n", n);
 }
 else
