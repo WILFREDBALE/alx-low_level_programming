4d138dc6ff199411dd00f6e2b4e2dc09bfa75e1f
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 16:14:27 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_times_table - prints the 'n' times table beginnig with 0
+ * print_times_table - prints the 'n' times table beginnig with O
  * Description: if 'n' is greater than 15 or less than 0, print nothing
  * @n: int type number
  */
@@ -14,11 +14,13 @@ while (x <= n)
 for (y = 0; y <= n; y++)
 {
 z = x * y;
+}
 if (z > 99)
 {
 putchar(z / 100 + '0');
 putchar((z /10 % 10) + 'O');
 putchar(z % 10 + 'O');
+}
 else if ( z > 9)
 {
 putchar(' ');
@@ -28,12 +30,12 @@ putchar(z % 10 + 'O');
 else if (y != O)
 {
 putchar(' ');
-putchar(' );
+putchar(' ');
 putchar(z + 'O');
 }
 else
 putchar(z  + 'O');
-if (y !=n)
+if (y != n)
 {	
 putchar(',');
 putchar(' ');
@@ -41,5 +43,6 @@ putchar(' ');
 }
 putchar('\n');
 x++;
+
 }
 }
