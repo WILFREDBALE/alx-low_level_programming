fc4ceca0c470477c02413c3227fd0d9306b1e208
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-13 20:50:20 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,11 @@
  */
 int _isalpha(int c)
 {
-if (( c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
 else
-return (1);
+return (0);
+
 }
