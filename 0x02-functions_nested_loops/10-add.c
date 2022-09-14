0180c4d89ee7e07d87f422cdb018304bd6149ee6
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 13:11:42 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @num1: first integer to be added
+ * @num2: second integer to be added
+ * Return: result of addition
+ */
+int add(int num1, int main2)
+{
+
+return (num1 + num2);
+
+}
