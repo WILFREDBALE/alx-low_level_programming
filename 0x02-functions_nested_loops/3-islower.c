a193361d70eb9a7dc7bae7a1c39ab1ff3c9649a5
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 02:17:41 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _islower - function checks for lowercase characters
- * &: this is the int to be used for the argument of the function
+ * @c: this is the int to be used for the argument of the function
  * Return: 0
  */
 int _islower(int c)
