d0dba1f870ce62dce3b9be2daf19c65b6555641c
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-14 08:01:18 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,17 @@ void times_table(void)
 	for (num = 0; num <=9; num = num + 1)
 	{
 		_putchar('0');
-		for (mult = 1; mult <= 9; mult + 1)
+		for (mult = 1; mult <= 9; mult++)
 	{
-		_putchat(',');
-		_putchar(' ');
+		_putchar(',');
+	        _putchar(' ');
 
 		product = num * mult;
 		if (product <= 9)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + '0');
-	      _putchar((prod % 10) + '0');
+	      _putchar((product % 10) + '0');
 	}
 	_putchar('\n');
 	}
