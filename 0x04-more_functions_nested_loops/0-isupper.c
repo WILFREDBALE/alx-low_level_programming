bf7ddb6498f54d6a2bacb850872f632f0f246bc9
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-15 15:22:40 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - this checks for uppercase charaacters
+ *
+ * @c: This is the uppercase character to be checked
+ *
+ * Return: 1 if character is uppercase, 0 if otherwise
+ */
+
+ist _issupper(int c)
+{
+if(c is >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  File - main.h
+ *  Author: BALE
+ * Description: This header contents prototypes for all functions used for this projects
+ */
+
+ist _putchar(char c);
+ *
+ *
+ *
+ #endif
