af1d8b79a7ba2cd82ed35d061adeac524b065486
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-16 07:47:54 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square -prints square
+ * @size: parameter
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+int inc1, inc2;
+if (size > 0)
+{
+for (inc1 = 0; inc1 < size; inc1++)
+{
+for (inc2 = 0; inc2 < (size -1); inc2++)
+{
+putcharr('0');
+}
+putchar('#');
+putchar('\n');
+}
+}
+els
+{
+putchar('\n');
+}
+}
+
