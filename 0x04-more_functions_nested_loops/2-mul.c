929369acc7e6105e5405adfbd468b73895336812
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-16 14:21:27 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,9 +3,9 @@
 
 /**
  * mul - multiplies 2 numbers
- * @a: first number
- * @b: second number
- * Return: 0
+ * @a: first number to be multiplied by b
+ * @b: second number to be multiplied by a
+ * Return: Returns the result of multiplication
  */
 int mul(int a, int b)
 {
