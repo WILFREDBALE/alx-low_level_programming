303b3defb74f996e9bcecb3c20673df7e2375527
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-16 15:54:37 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,31 @@
 int main(void)
 {
 int num = 1;
-while (num++ < 100)
+while (num < 101)
 {
-if ((num % 3 == 0) && (num % s == 0))
+if ((num % 3 == 0) && (num % 5 == 0))
 {
-printf("FizzBuzz ");
+printf("%s", "FizzBuzz");
 }
 else if ((num % 3) == 0)
 {
-printf("Fizz ");
+printf("%s", "Fizz");
 }
 else if ((num % 5) == 0)
 {
-if (num != 100)
-{
-printf("Buzz ");
+printf("%s", "Buzz");
 }
 else
+printf("%d", num);
+}
+if (num != 100)
+{
+printf(" ");
+}
+num++;
+}
+printf("\n");
 return (0);
-
 }
 
 
