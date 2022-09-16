b1bd83773a972dc1c892eca8a5953aa5b7b23057
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-16 14:10:48 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
  * mul - multiplies two numbers
  * @a: first number
  * @b: second number
- * Return: return result
+ * Return: return result of multiplication
  */
 int mul(int a, int b)
 {
