762b681175c59716e425b86f052e7e0f85d2442b
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-19 16:31:48 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a strings
+ * @str: string
+ * Description: prints a string
+ * on sucess: return no error
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+while(*(str + i) != '\0')
+{
+putchar(*(str + i);
+	i++;
+}
+putchar(10);
+}
