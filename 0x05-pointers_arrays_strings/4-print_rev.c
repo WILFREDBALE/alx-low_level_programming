7b7d5c9cf25ad4c86048642b1657306f7c30fa71
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-20 00:11:51 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse order
+ * @s: string to reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int len = strlen(s);
+while (len --)
+putchar(*(s + len));
+putchar(10);
+}
