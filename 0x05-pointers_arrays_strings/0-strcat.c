abe783a56d92c37857510f3eddd795186e093834
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-21 02:55:05 -0700

    0-strcat.c

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h>
+
+/**
+ * *_strcat - this functions concatenates two strings
+ * @dest: string to which src is appended
+ * @src: string to be appended to dest
+ * Return: value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int a = 0;
+int b = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+b++;
+i++;
+}
+dest[a] = '\0'
+return (dest);
+
+}
+
