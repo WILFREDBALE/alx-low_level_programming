0338a59385f33837f3895c5b07b3b9cd74fd49f1
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-21 12:02:39 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
-#include <stdio.h>                                                                                           
+#include <stdio.h>                                                                
 #include <string.h>                                                                                          
-#include "main.h>                                                                                            
+#include "main.h"                                                                                            
                                                                                                              
 /**                                                                                                          
- * *_strcat - this functions concatenates two strings                                                        
+ *  *_strcat - this functions concatenates two strings                                                        
  * @dest: string to which src is appended                                                                    
  * @src: string to be appended to dest                                                                       
  * Return: value of dest                                                                                     
  */                                                                                                         
-                                                                                                             
+                                                                                                            
 char *_strcat(char *dest, char *src)                                                                         
 {                                                                                                            
 int a = 0;                                                                                                   
