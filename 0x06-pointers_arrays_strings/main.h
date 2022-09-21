71f953b0fd2f449a5a19e022e8d472307d0a99a0
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-21 14:43:23 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,12 @@
 
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+nt _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+ar *cap_string(char *);
 
 
 #endif
