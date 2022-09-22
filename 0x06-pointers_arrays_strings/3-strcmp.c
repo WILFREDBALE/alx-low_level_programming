b696a4d96c898784445e1cafe5a7648679192468
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-22 08:58:32 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,7 @@
  * @s2: second string
  * Return: returns 0 if s1 == s2, negative number if s1 < s2,
  * and positive number if s1 > s2.
- */
- 
+ */ 
 int _strcmp(char *s1, char *s2)
 {
 int a = 0;
@@ -21,6 +20,11 @@ break;
 else if (s1[a] == '\0')
 {
 b = s2[a];
+break;
+}
+else if (s2[a] == '\0')
+{
+b = s1[a];
 break;
 }
 else if (s1[a] != s2[a])
