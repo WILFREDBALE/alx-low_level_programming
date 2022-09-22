930344e4b834237f28ea44be96267968731615b8
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-22 01:43:12 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int a;
-for (a = 0; a < n; && src[a] != '\0'; a++)
+for (a = 0; a < n; &&src[a] != '\0'; a++)
 dest[a] = scr[a];
 for ( ; a < n; a++)
 dest[a] = '\0';
-return(dest);
+return (dest);
 
 }
