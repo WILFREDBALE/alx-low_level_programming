47d5beebccab36938e12265823829284d6cb385a
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-22 06:07:47 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes words
+ * @str: pointer
+ * Return: capitalised string
+ */
+
+char *cap_string(char *str)
+{
+char sep[] = ",\t;\n; .!?\"(0{]";
+int flag, i, ii;
+for (i = o; str[i] != '\0'; i++)
+{
+flag = 0;
+if (i == 0)
+{
+flag = 1;
+}
+else
+{
+for (ii = o; sep[ii] != '\0'; ii++)
+{
+if (str[i - 1] == sep[ii])
+{
+flag = 1;
+break;
+}
+}
+}
+if (flag == 1)
+{
+if (str[i] <= 'z' && str[i] >= 'a')
+{
+str[i] -= ('a' - 'A');
+}
+}
+}
+return (str);
+
+}
