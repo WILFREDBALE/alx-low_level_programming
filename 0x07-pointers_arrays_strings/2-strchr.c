566187ab77de91a7df7f904801c0fcf5a0977a48
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-27 02:56:48 -0700

    /** * _strchr - locates a character * @s: string to be searched * @c: target * Return: pointer to first occurence of c or null if char not found */ char *_strchr(char *s, char c) { while (*s) { if (c == *s) { return (s); } s++; } if (cd 0x07-pointers_arrays_strings) return (s); return (NULL); }clear clear
    '

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * * _strchr - locates a character
+ * * @s: string to be searched
+ * * @c: target
+ * * Return: pointer to first occurence of c or null if char not found
+ * */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (c == *s)
+{                                                                           
+return (s);
+}
+s++;
+if (!c)
+return (s);
+
+return (NULL);
+}
+
