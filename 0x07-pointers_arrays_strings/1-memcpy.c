d9842fbad2cabea4a698a822af92dd0e34932b3a
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-27 06:33:49 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,13 @@
  * @src: source str
  * @n: number of bytes to copy
  * Return: pointer to dest
- *       */
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-char *ptr = dest;
-while (n--)
-*dest++ = *src++;
 
-return (ptr);
+unsigned int i;
+for (i = 0; i < n; i++)
+*(dest + i) = *(src + i);
+return (dest);
+
 }
-
