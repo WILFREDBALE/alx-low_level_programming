5902c7717d9a0e6a8874169d40fe14cb5556ad04
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-27 07:04:20 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-*(s + i) = b;
+int i;
+for (i = 0; n > 0; i++)
+{
+s[i] = b;
+n--;
 return (s);
 
 }
