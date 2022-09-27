5bd835143d7e0e89086e464c0a68a79f3358a231
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-27 06:08:26 -0700

    { unsigned int i;
    for  (i = 0; i < n; i++)
    *(s + i) = b;
    
    return (s);

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-char *ptr;
-*ptr = s;
-while (n--)
-*s++ = b;
-return (ptr);
+unsigned int i;
+for  (i = 0; i < n; i++)
+*(s + i) = b;
+return (s);
 
 }
-
