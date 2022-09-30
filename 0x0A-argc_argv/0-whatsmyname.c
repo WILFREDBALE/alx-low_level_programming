f3c86acb88a521231ad04027baa53722cccbca6d
WILFRED BALE NANKPAK <nankpakbalewilfred@gmail.com>
2022-09-30 09:18:24 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - is a function that prints the name of the file
- * @argc: is an argument counter for char
- * @argv: is an argument value for char
+ * main - It is a function that prints the name of the file
+ * @argc: It is an argument counter for char
+ * @argv: It is an argument value for char
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
